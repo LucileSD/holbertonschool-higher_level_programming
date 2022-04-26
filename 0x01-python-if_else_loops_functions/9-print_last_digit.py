21ba37bad79a10b3b1c23c24a08ab9cc8c1739d2
LucileSD <4316@holbertonschool.com>
2022-04-26 12:59:30 +0000

    print and return

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,6 @@
 def print_last_digit(number):
     last_dig = number % 10
     if number < 0:
-        last_dig = (-number % 10) * -1
+        last_dig = -number % 10
+    print("{}".format(last_dig), end="")
     return last_dig
