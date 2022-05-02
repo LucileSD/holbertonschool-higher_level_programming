d648d47a852704541a8dd198ae644c09636d23ed
LucileSD <4316@holbertonschool.com>
2022-05-02 13:38:15 +0000

    without max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,4 +3,8 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        return max(my_list)
+        biggest = my_list[0]
+        for i in my_list:
+            if i > biggest:
+                biggest = i
+        return biggest
