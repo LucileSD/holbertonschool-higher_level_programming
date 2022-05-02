32708fce2573b07189f8c348a168ef6f49e2a83c
LucileSD <4316@holbertonschool.com>
2022-05-02 13:06:16 +0000

    pycode

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,6 @@ def multiple_returns(sentence):
     create_tuple = tuple(sentence)
     if not sentence:
         create_tuple = (None, )
-    
     lenght = len(sentence)
     first_letter = create_tuple[0]
     return (lenght, first_letter)
