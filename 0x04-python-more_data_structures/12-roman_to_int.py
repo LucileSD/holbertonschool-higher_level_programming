d756f9b51a81f94c38ab4b9abba4ad97939e0eb1
LucileSD <4316@holbertonschool.com>
2022-05-04 15:34:16 +0000

    instance

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     if roman_string:
         if roman_string and isinstance(roman_string, str):
-            one = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+            one = {'I': 1, 'V': 5, 'X': 10,
+                   'L': 50, 'C': 100, 'D': 500, 'M': 1000}
             two = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
             result = 0
             i = 0
