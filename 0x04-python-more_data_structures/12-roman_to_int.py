95c07fd8e929134e43bd2d8dca97ca4c1e3436f6
LucileSD <4316@holbertonschool.com>
2022-05-04 15:29:48 +0000

    instance

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string and isinstance(roman_string, str):
-        one = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        two = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
-        result = 0
-        i = 0
-        roman_concate = ""
-        while i < len(roman_string):
-            if i + 1 < len(roman_string):
-                roman_concate = roman_string[i] + roman_string[i + 1]
-            if roman_string in two:
-                result += two[roman_concate]
+    if roman_string:
+        if roman_string and isinstance(roman_string, str):
+            one = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+            two = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+            result = 0
+            i = 0
+            suit = ""
+            while i < len(roman_string):
+                if i + 1 < len(roman_string):
+                    suit = roman_string[i] + roman_string[i + 1]
+                if roman_string in two:
+                    result += two[suit]
+                    i += 1
+                elif roman_string[i] in one:
+                    result += one[roman_string[i]]
                 i += 1
-            elif roman_string[i] in one:
-                result += one[roman_string[i]]
-            i += 1
-        return result
+            return result
+        else:
+            return 0
     else:
         return 0
