aaa4786f4ae2c07ed2f1a15235c285f827ca7236
LucileSD <4316@holbertonschool.com>
2022-05-16 11:43:10 +0000

    message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+a function that raises a name exception with a message
+Arguments
+    message: the string to print
+"""
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
