ce4d49fce916343e381dfff825aae349ea9f2b5e
LucileSD <4316@holbertonschool.com>
2022-05-20 15:02:11 +0000

    indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """prints a text with 2 new lines after each of
+    these characters: ., ? and :"""
+    i = 0
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    while i < len(text):
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print(f"{text[i]}\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+        else:
+            print(f"{text[i]}", end="")
+            i += 1
