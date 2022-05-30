f935ba0a41cdd5882776775f230fafb1316858c8
LucileSD <4316@holbertonschool.com>
2022-05-30 15:00:20 +0000

    area width

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+    Square class file
+"""
+
+
+class Square(Rectangle):
+    """
+        defines a square
+    """
+    def __init__(self, size):
+        """
+            initiate a square
+            args:
+                size
+        """
+        super().integer_validator("size", size)
+        self.__width = size
+        self.__height = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+            calculates area of a square
+        """
+        return self.__width ** 2
+
+    def __str__(self):
+        """
+            prints the square description
+        """
+        return (f"[Square] {self.__width}/{self.__height}")
