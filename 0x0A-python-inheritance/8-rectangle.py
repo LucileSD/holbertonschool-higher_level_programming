c34342197070069052867308efd271a1e4214dc3
LucileSD <4316@holbertonschool.com>
2022-05-30 13:35:24 +0000

    type

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -25,3 +25,20 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+        create a class Rectangle which inherit of the baseclass BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+            initiate the rectangle
+            args:
+                width
+                height
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
