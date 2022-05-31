dee17168410e0b64a1a6d8dd049a8bb0249eddc6
LucileSD <4316@holbertonschool.com>
2022-05-31 13:22:13 +0000

    setattr

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+    student class file
+"""
+
+
+class Student:
+    """
+        class to define a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            initiate the class
+            args:
+                first_name
+                last_name
+                age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            a function that retrieves a dictionary representation
+            of a Student instance
+            args:
+                attrs: attributes
+            return:
+                the dictionary description with simple data structure
+        """
+        if type(attrs) is list:
+            x = {}
+            for at in attrs:
+                if hasattr(self, at):
+                    x[at] = getattr(self, at)
+            return x
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+            replaces all attributes of the Student instance
+            args:
+                json: a dictionary
+        """
+        for n, v in json.items():
+            setattr(self, n, v)
