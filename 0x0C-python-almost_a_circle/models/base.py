d3e1a237da669e8495d3d6406396a6db629fa9eb
LucileSD <4316@holbertonschool.com>
2022-06-03 12:20:59 +0000

    json dumps

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """
+    import module json
+"""
+import json
+
+"""
     Base class file
 """
+
+
 class Base:
     """
         base class
@@ -19,3 +26,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+            the JSON string representation of list_dictionaries
+            args:
+                list_dictionaries: list of dictionaries
+            return: JSON string
+        """
+        return json.dumps(list_dictionaries)
