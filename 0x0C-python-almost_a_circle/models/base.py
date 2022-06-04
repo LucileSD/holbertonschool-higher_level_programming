7c915121d9668e84f88ac6afdf26e44f6cf9c4cb
LucileSD <4316@holbertonschool.com>
2022-06-04 09:01:14 +0000

    save

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@
 """
 import json
 
+
 """
     Base class file
 """
@@ -27,6 +28,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
             the JSON string representation of list_dictionaries
@@ -35,3 +37,23 @@ class Base:
             return: JSON string
         """
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+            writes the JSON string representation of list_objs to a file
+            args:
+                list_objs: a list of instances who inherits of Base
+        """
+        list_dict = []
+        str = ""
+
+        if list_objs is None:
+            list_objs = []
+        for inst in list_objs:
+            list_dict.append(inst.to_dictionary())
+
+        str = cls.to_json_string(list_dict)
+
+        with open(cls.__name__ + ".json", "w", encoding="utf-8") as buf:
+            buf.write(str)
