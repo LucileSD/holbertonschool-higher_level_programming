3cce2cb68346f46cd4149c9ee33569c044931036
LucileSD <4316@holbertonschool.com>
2022-07-05 12:09:38 +0000

    group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
--- list the number of records with the sasme score
+-- list the number of records with the same score
 SELECT score, COUNT(*) as number FROM second_table
 GROUP BY score
 ORDER BY number DESC;
