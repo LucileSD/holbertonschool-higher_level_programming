9768df4fdafa59a9fc140e1bdf8a453ad2e97319
LucileSD <4316@holbertonschool.com>
2022-08-01 11:11:08 +0000

    loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/**
+ *  prints 3 lines by using an array of string and a loop
+ */
+const threeLine = ['C is fun\nPython is cool\nJavaScript is amazing'];
+let idx;
+for (idx = 0; idx < threeLine.length; idx++) {
+  console.log(threeLine[idx]);
+}
