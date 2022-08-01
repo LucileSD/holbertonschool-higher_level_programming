a70304de0a000e882a1b0e571ac3181638cca718
LucileSD <4316@holbertonschool.com>
2022-08-01 08:02:03 +0000

    strings

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/*
+/**
 * script to write a string
 */
 const myVar = 'JavaScript is amazing';
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/**
+ * a script that prints 3 lines
+ */
+const threeLine = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(threeLine);
