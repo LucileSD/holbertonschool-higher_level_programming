199daf44f0bd54057ea30122ce6a52b559a31a3f
LucileSD <4316@holbertonschool.com>
2022-08-01 11:01:25 +0000

    not a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,5 +6,5 @@
 if (!process.argv[2]) console.log('Not a number');
 else if (process.argv[2]) {
   if (isNaN(process.argv[2])) console.log('Not a number');
-  else console.log(parseInt(process.argv[2]));
+  else console.log('My number: ' + parseInt(process.argv[2]));
 }
