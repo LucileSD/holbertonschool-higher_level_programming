55514eaec75003cdd73795451bf7b604efc7fbcb
LucileSD <4316@holbertonschool.com>
2022-08-04 09:24:43 +0000

    if name

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,15 +6,16 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host='localhost',
-                     port=3306,
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
-rows = cur.fetchall()
-for line in rows:
-    print(f"{line}")
-cur.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    rows = cur.fetchall()
+    for line in rows:
+        print(f"{line}")
+    cur.close()
+    db.close()
