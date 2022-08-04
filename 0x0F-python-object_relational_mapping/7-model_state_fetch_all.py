9042e205e15a8faa444fd0ee65d0c8bb7d500068
LucileSD <4316@holbertonschool.com>
2022-08-04 15:21:27 +0000

    engine

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    import
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3],
+            pool_pre_ping=True
+            ),
+    )
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for res in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(res.id, res.name))
+    session.close()
