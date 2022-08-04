0cca63c0a647f4c6029e6ef96a94acdd04d839e2
LucileSD <4316@holbertonschool.com>
2022-08-04 09:11:04 +0000

    port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,8 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host='localhost',
+db = MySQLdb.connect(host="localhost",
+                     port=3306,
                      user=sys.argv[1],
                      passwd=sys.argv[2],
                      db=sys.argv[3])
