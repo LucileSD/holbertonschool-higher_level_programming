23283d2275186a0bc69fc85d95d128ff84268e4a
LucileSD <4316@holbertonschool.com>
2022-08-04 14:47:10 +0000

    first sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    import class Base, sqlalchemy
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+"""
+    state class file
+"""
+
+
+class State(Base):
+    """
+        creation of State class that inherit of Base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
