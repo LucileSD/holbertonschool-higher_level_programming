3f701241fc42de7aea7916427bdd156ba39bca7c
LucileSD <4316@holbertonschool.com>
2022-08-05 10:30:01 +0000

    join two class

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,21 +2,21 @@
 """
     import class Base, sqlalchemy
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
 
-
-Base = declarative_base()
 """
-    state class file
+    city class file
 """
 
 
-class State(Base):
+class City(Base):
     """
-        creation of State class that inherit of Base
+        creation of City class that inherit of Base
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False,
                 unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
