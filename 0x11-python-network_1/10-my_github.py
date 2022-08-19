c95abce450f3e645bc0a8b02b72239239572807e
LucileSD <4316@holbertonschool.com>
2022-08-19 12:55:22 +0000

    try

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    import Python module for fetching URLs
+"""
+import requests
+import sys
+
+
+"""
+    takes your GitHub credentials (username and password) and uses
+    the GitHub API to display your id
+"""
+
+
+if __name__ == "__main__":
+    req = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+    res = req.json()
+    try:
+        print(res['id'])
+    except KeyError:
+        print("None")
