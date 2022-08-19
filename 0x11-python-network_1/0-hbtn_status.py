5614768693ae43c0a0ed046d4aa69bfdb5d88b24
LucileSD <4316@holbertonschool.com>
2022-08-19 10:41:41 +0000

    import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """
     import Python module for fetching URLs
 """
-import urllib
+import urllib.request
 
 
 """
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         body = response.read()
     print("Body response:")
-    print(f"\t- type: {type}")
+    print(f"\t- type: {type(body)}")
     print(f"\t- content: {body}")
     print(f"\t- utf8 content: {body.decode('utf-8')}")
