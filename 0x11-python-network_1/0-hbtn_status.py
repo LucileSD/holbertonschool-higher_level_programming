bf1583ec3cc5cca6e2d767dc8cac5939d3bcbd55
LucileSD <4316@holbertonschool.com>
2022-08-19 10:47:10 +0000

    tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,10 +10,9 @@ import urllib.request
 """
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-    print("Body response:")
-    print(f"    - type: {type(html)}")
-    print(f"    - content: {html}")
-    print(f"    - utf8 content: {html.decode('utf-8')}")
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+print("Body response:")
+print(f"    - type: {type(html)}")
+print(f"    - content: {html}")
+print(f"    - utf8 content: {html.decode('utf-8')}")
