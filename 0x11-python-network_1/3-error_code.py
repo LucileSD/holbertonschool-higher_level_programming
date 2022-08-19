6ad8afc64bfe1d3f4189d1f996ec15dab28952b1
LucileSD <4316@holbertonschool.com>
2022-08-19 13:00:12 +0000

    score

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -16,6 +16,6 @@ from urllib.error import HTTPError
 if __name__ == "__main__":
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode('utf_8'))
+            print(response.read().decode('utf-8'))
     except HTTPError as error:
         print("Error code: {}".format(error.code))
