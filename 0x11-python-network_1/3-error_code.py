a6a9eb60e7dfb7c5a3c7f3c32e5366ab2f34ae40
LucileSD <4316@holbertonschool.com>
2022-08-19 11:00:58 +0000

    error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    import Python module for fetching URLs
+"""
+import urllib.request
+import sys
+
+
+"""
+    sends a request to the URL and displays the body of the response
+    (decoded in utf-8)
+"""
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf_8'))
+    except urllib.request.error.HTTPError as error:
+        print(f"Error code: {error.code}")
