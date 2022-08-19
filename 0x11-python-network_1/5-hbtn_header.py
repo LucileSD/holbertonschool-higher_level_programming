798992b20de299093f91ed09ce79c5d00be6fff4
LucileSD <4316@holbertonschool.com>
2022-08-19 11:29:35 +0000

    request id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    import Python module for fetching URLs
+"""
+import requests
+import sys
+
+
+"""
+    fetches sends a request to the URL and displays the value of the variable
+    X-Request-Id in the response header
+"""
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
