e7330784856af478749ad6f1dc82d5a6c467c2db
LucileSD <4316@holbertonschool.com>
2022-09-13 08:35:51 +0000

    get data from an url with axios

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/* prints the title of a Star Wars movie where the
+  episode number matches a given integer
+*/
+const axios = require('axios');
+
+axios({
+  method: 'GET',
+  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2]
+})
+  .then((response) => {
+    console.log(response.data.title);
+  },
+  (error) => {
+    console.log('code: ' + error.response.status);
+  });
