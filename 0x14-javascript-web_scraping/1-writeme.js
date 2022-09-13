a4d71946cd4580fbad93147fb204d935724e589d
LucileSD <4316@holbertonschool.com>
2022-09-13 08:02:21 +0000

    write in a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* writes a string to a file */
+const fs = require('fs');
+
+const content = process.argv[3];
+
+fs.writeFile(process.argv[2], content, 'utf-8', err => {
+  if (err) {
+    console.error(err);
+  }
+});
